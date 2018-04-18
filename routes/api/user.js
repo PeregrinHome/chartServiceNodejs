19ea1e44c3e75a58f02c512b601a08f32a385a4d
cvaize <cvaize@gmail.com>
2018-04-18 10:16:57 +0500

    make backand user

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/user');
+const mongoose = require('mongoose');
 
-const getRoute = require('./user/get');
-const postRoute = require('./user/post');
-const putRoute = require('./user/put');
-const deleteRoute = require('./user/delete');
+router.get('/', function(req, res, next) {
 
-router.use('/', getRoute);
-router.use('/', postRoute);
-router.use('/', putRoute);
-router.use('/', deleteRoute);
+    User.findOne({ name: 'Dmitry'}, function (err, user) {
+        if (err) return handleError(err);
+        console.log(user);
+        res.status(200);
+        res.send('respond with a resource ' + user.email);
+    });
+});
+
+router.post('/', function(req, res, next) {
+
+    // Добавить валидацию
+
+    const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        login: req.body.login,
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save().then(result =>{
+        console.log('Result in user_routes', result)
+    }).catch(errors => {
+        console.log('Errors in user_routes', errors)
+    });
+
+    res.send('respond with a resource /user');
+});
+
+router.post('/charts', function(req, res, next) {
+
+    // Добавить валидацию
+
+
+
+    res.send('respond with a resource /user');
+});
+
+router.put('/', function(req, res, next) {
+    res.send('respond with a resource /user');
+});
+
+router.delete('/', function(req, res, next) {
+    res.send('respond with a resource /user');
+});
 
 
 module.exports = router;
